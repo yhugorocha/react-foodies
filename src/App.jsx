d3177797ceecfa3652a48a3e1558b3419206fa10
Hugo Silva <jose.silva@extreme.digital>
2026-02-24 15:27:30 -0300

    refactor App component structure and update import statements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Menubar } from "./components/Menubar/Menubar";
+import { Routes, Route } from "react-router-dom";
+import { Home } from './pages/Home/Home';
+import { ContactUs } from './pages/ContactUs/ContactUs';
+import { ExploreFood } from './pages/ExploreFood/ExploreFood';
+import { Header } from "./components/Header/Header";
+
 export const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Menubar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/contact' element={<ContactUs />}></Route>
+        <Route path='/explore' element={<ExploreFood />}></Route>
+      </Routes>
+    </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import { App } from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
