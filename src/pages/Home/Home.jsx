287fcbf665096d1e77d320cb626043a193879226
Hugo Silva <jose.silva@extreme.digital>
2026-02-24 15:26:59 -0300

    add Home component with header and explore menu

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import { ExploreMenu } from "../../components/ExploreMenu/ExploreMenu"
+import { Header } from "../../components/Header/Header"
+
+export const Home = () => {
+    return (
+        <main className="container">
+            <Header />
+            <ExploreMenu />
+        </main>
+    )
+}
