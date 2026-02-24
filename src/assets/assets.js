1ce5996bf238b12e983e0a41622269b68036bded
Hugo Silva <jose.silva@extreme.digital>
2026-02-24 15:26:51 -0300

    add asset images and categories for food items

---
+++ src/assets/assets.js
@@ -0,0 +1,45 @@
+import logo from './logo.png';
+import card from './card.png';
+import cake from './cake.png';
+import icecream from './icecream.png';
+import pizza from './pizza.png';
+import salad from './salad.png';
+import salgados from './salgados.png';
+import burguer from './burguer.png';
+import drinks from './drinks.png';
+
+export const assets = {
+    logo,
+    card
+};
+
+export const categories = [
+    {
+        category: 'Doces',
+        icon: cake
+    },
+    {
+        category: 'Sorvetes',
+        icon: icecream
+    },
+    {
+        category: 'Pizzas',
+        icon: pizza
+    },
+    {
+        category: 'Saladas',
+        icon: salad
+    },
+    {
+        category: 'Salgados',
+        icon: salgados
+    },
+    {
+        category: 'Hambúrguers',
+        icon: burguer
+    },
+    {
+        category: 'Bebidas',
+        icon: drinks
+    }
+]
